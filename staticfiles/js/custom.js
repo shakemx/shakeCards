2939b7664d155bcc5eed0fd82e9d63d8d7b94e85
Guillermo Pineda <guillermopineda@Mac-mini-de-Guillermo.local>
2024-01-07 23:26:04 -0600

    first home page

--- staticfiles/js/custom.js
+++ staticfiles/js/custom.js
@@ -2,3 +2,15 @@ $(document).on('show.bs.collapse hide.bs.collapse', '.collapse', function(e) {
     e.stopPropagation();
     console.log('event triggered');
 });
+
+
+function navbarMotion() {
+    var element = $("#listaNav");
+    var toogle = $("#menuToogle");
+  
+      element.toggleClass("hideNav");
+      element.toggleClass("showNav");
+      toogle.toggleClass("menuShow");
+      toogle.toggleClass("menuHide");
+  
+  }
